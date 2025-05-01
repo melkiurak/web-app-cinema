134fa9e75cd7d81c75eb169fda77d6943da76396
unknown <melkiurak3209@gmail.com>
2025-05-01 12:45:18 +0300

    add font

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,9 @@
 export const Header = () => {
     return <div className="container">
-        <h1>header</h1>
+        <h1 className="font-black-Qanelas">Я с шрифтом</h1>
+        <h1 className="font-bold-Qanelas">Я с шрифтом</h1>
+        <h1 className="font-medium-Qanelas">Я с шрифтом</h1>
+        <h1 className="font-regular-Qanelas ">Я с шрифтом</h1>
+        <h1 className="font-semi-bold-Qanelas">Я без шрифтом</h1>
     </div>
 }
