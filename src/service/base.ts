0bd7efae772351a072e1613bd3240cb6e578e5af
unknown <melkiurak3209@gmail.com>
2025-05-03 14:21:04 +0300

    .

--- src/service/base.ts
+++
@@ -1,9 +0,0 @@
-
-export const options = {
-    method: 'GET',
-    url: 'https://imdb236.p.rapidapi.com/imdb/lowest-rated-movies',
-    headers: {
-      'x-rapidapi-key': '814c80cb99msh4298e841ecea3fap1ca37bjsnd1f92fdd0c9a',
-      'x-rapidapi-host': 'imdb236.p.rapidapi.com'
-    }
-  };
